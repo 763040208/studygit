35ec100d2c597cfd2931c09c8cf8a894f16df3e8
wanghongfei <jiangyong763040208@qq.com>
2018-07-13 13:58:52 +0800

    跑了

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("江南皮革厂");
         System.out.print("倒闭了");
+        System.out.println("待着小姨子跑了");
+        System.out.println("跑就跑了");
     }
 }
